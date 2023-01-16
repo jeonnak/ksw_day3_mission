9e4e050d9a95ceb87c6c0242772d2049803949a0
JNK <deal6789@naver.com>
2023-01-16 16:47:57 +0900

    day03 5.6

--- day03_mission.py
+++ day03_mission.py
@@ -1,29 +1,12 @@
-'''
-5.4, 5.5
-'''
+# 5.6
 
-letter = '''\tDear {0} {1},\n
-\tThank you for your letter. We are sorry that our {2} {3} in your
-{4}. Please note that it should never be used in a {4}, especially near any
-{5}.\n\n
-\tSend us your receipt and {6} for shipping and handling. We will send you
-another {2} that, in our tests, is {7}% less likely to have {3}. \n\n
-\tThank you for your support.
-\tSincerely,
-\t{8}
-\t{9}
-'''
+# an English submarine = 'Boaty McBoatface'
+# an Australian racehorse = 'Horsey McHorseface'
+# a Swedish train = 'Trainy McTrainface'
 
-salutation = 'professor'
-name = 'minsu'
-product = 'pencil'
-verbed = 'losted'
-room = 'living room'
-animals = 'dog'
-amount = 'love'
-percent = '20'
-spokesman = 'mina'
-job_title = 'student'
-
-print(letter.format(salutation, name, product, verbed, room, animals, amount, percent, spokesman, job_title))
-
+duck = "Ducky McDuckface"
+gourd = "Gourdy McGourdface"
+spitz = "Spitzy McSpitzface"
+print('''an English submarine 이름은 (%s)로 지어졌다.
+an Australian racehorse 이름은 (%s)로 지어졌다. 
+a Swedish train 이름은 (%s)로 지어졌다.''' % (duck, gourd, spitz))
